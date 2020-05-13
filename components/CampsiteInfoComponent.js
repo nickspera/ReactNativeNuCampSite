0c73f882dcf630330cc289c74f2526034064c01f
Nick Spera <nickaspera@gmail.com>
2020-05-13 13:14:35 -0400

    Reister Component and Camera Permissions

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -27,9 +27,8 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < - 200) ? true : false;
-    //
     const recognizeComment = ({dx}) => (dx > 200) ? true : false;
-    //
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -56,14 +55,23 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
-            //
             } else if (recognizeComment(gestureState)){
                 props.onShowModal();
             }
-            
+
             return true;
         }
-    })
+    });
+
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        }, {
+            dialogTitle: 'Share' + title
+        });
+    };
 
     if (campsite) {
         return (
@@ -95,8 +103,17 @@ function RenderCampsite(props) {
                             color='#5637DD'
                             raised
                             reverse
-                            onPress={() => props.onShowModal()}
                             style={styles.cardItem}
+                            onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            style={styles.cardItem} 
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
                         />
                     </View>
                 </Card>
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.0.20:3001/';
+export const baseUrl = 'http://10.0.0.211:3001/';
